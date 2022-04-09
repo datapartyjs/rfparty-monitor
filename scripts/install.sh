30878691134679af5088662d14e7acbf64d3fe51
nullagent <null@nullagent.com>
2022-04-09 22:54:08 +0000

    permissions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Setting node permissions"
-setcap cap_net_raw+eip $(eval readlink -f `which node`)
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 
 
 echo "Linking directory"
 
 dir=$(dirname "$(readlink -f "$0")")
-ln -s $(dirname $dir) /usr/lib/rfparty-monitor
+sudo ln -s $(dirname $dir) /usr/lib/rfparty-monitor
 
 echo "Linking executables"
-ln -s $dir/../bin/rfparty-info /usr/sbin/
-ln -s $dir/../bin/rfparty-monitor /usr/sbin/
+sudo ln -s $dir/../bin/rfparty-info /usr/sbin/
+sudo ln -s $dir/../bin/rfparty-monitor /usr/sbin/
 
 echo "Installing service files"
-cp $dir/systemd/rfparty-monitor.service /etc/systemd/system/
+sudo cp $dir/systemd/rfparty-monitor.service /etc/systemd/system/
 
 echo "Starting service 🤘"
-systemctl daemon-reload
-systemctl enable rfparty-monitor.service
-systemctl start rfparty-monitor.service
+sudo systemctl daemon-reload
+sudo systemctl enable rfparty-monitor.service
+sudo systemctl start rfparty-monitor.service
