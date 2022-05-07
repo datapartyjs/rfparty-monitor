58edb25a3c9d259c3c83d633a005fd1d4f73364f
7bit <662932+sevenbitbyte@users.noreply.github.com>
2022-05-07 15:14:45 -0700

    Update install-requirements.sh

--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install build-essential bluetooth bluez bluez-hcidump libbluetooth-dev libudev-dev gpsd gpsd-clients gpsbabel python-gps wireless-tools aircrack-ng libpcap-dev
+sudo apt install build-essential bluetooth bluez bluez-hcidump libbluetooth-dev libudev-dev gpsd gpsd-clients gpsbabel wireless-tools aircrack-ng libpcap-dev
